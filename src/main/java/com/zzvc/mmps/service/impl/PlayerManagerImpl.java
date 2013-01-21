f3c55903481b15bb814d063703bf7cc8c6fc043e
cuihbin <cuihbin@gmail.com>
2013-01-21 16:17:42 +0800

    Use annotation for service

--- src/main/java/com/zzvc/mmps/service/impl/PlayerManagerImpl.java
+++ src/main/java/com/zzvc/mmps/service/impl/PlayerManagerImpl.java
@@ -5,11 +5,13 @@ import java.util.List;
 
 import org.appfuse.service.impl.GenericManagerImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.zzvc.mmps.dao.PlayerDao;
 import com.zzvc.mmps.model.Player;
 import com.zzvc.mmps.service.PlayerManager;
 
+@Service("playerManager")
 public class PlayerManagerImpl extends GenericManagerImpl<Player, Long> implements PlayerManager {
 	private static final int MINUTES_BEFORE_PLAYER_HEARTBEAT_FAIL = 30;
 	
